7f3fba4574c4936786943c5ea9a6c26f667197ae
Suvarna221B <suvarnasivadas@gmail.com>
2021-09-03 19:08:05 +0530

    todomaticApp updated

--- src/index.js
+++ src/index.js
@@ -3,7 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+const DATA = [
+  { id: "todo-0", name: "Eat", completed: true },
+  { id: "todo-1", name: "Sleep", completed: false },
+  { id: "todo-2", name: "Repeat", completed: false }
+];
+
 ReactDOM.render(
-    <App subject = "Su"/>,
+    <App tasks={DATA}/>,
   document.getElementById('root')
 );
